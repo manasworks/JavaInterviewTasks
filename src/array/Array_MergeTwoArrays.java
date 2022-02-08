ce01db7937cddfd9f19bc1e0e2dcc45be3f91548
elvirka7 <elvirka7@gmail.com>
2022-02-07 21:36:47 -0500

    Array_MergeTwoArrays task completed

--- src/array/Array_MergeTwoArrays.java
+++ src/array/Array_MergeTwoArrays.java
@@ -1,5 +1,7 @@
 package array;
 
+import java.util.Arrays;
+
 public class Array_MergeTwoArrays {
 
     public static void main(String[] args) {
@@ -7,6 +9,22 @@ public class Array_MergeTwoArrays {
         int[] arr1 = {1,2,3,4,5};
         int[] arr2 = {6,7,8,9,10};
 
+        int [] mergedArray = mergedArray(arr1, arr2);
+        System.out.println(Arrays.toString(mergedArray));
     }
-}
+    
+    public static int [] mergedArray(int [] arr1, int [] arr2){
+        int [] mergedArray = new int[arr1.length+arr2.length];
+        int j=0;
+        for (int each : arr1) {
+            mergedArray[j++]=each;
+        }
+        for (int each : arr2) {
+            mergedArray[j++]=each;
+        }
+        return mergedArray;
+
+        }
+    }
+
 // Write a return method that can concat two arrays
