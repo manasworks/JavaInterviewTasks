186157e459a1facb63bd334ec3fac7f1fe3e6567
elvirka7 <elvirka7@gmail.com>
2022-02-08 10:19:05 -0500

    Array_MoveAllZerosToTheEnd task completed

--- src/array/Array_MoveAllZerosToTheEnd.java
+++ src/array/Array_MoveAllZerosToTheEnd.java
@@ -1,11 +1,27 @@
 package array;
 
+import java.util.Arrays;
+
 public class Array_MoveAllZerosToTheEnd {
 
-    public static void main(String[] args) {
+    public static int[] moveZeroesToEnd(int[] arr) {
 
-        int[] arr = {0,5,6,9,7,2,-5,0,0,2,2-99,22,0};
+        int[] rev = new int[arr.length];
+        int j = 0;
+        for (int each : arr) {
+            if (each != 0) {
+                rev[j++] = each;
+            }
+        }
+        return rev;
 
     }
+
+    public static void main(String[] args) {
+        int[] arr = {0, 5, 6, 9, 7, 2, -5, 0, 0, 2, -99, 22, 0};
+        System.out.println(Arrays.toString(moveZeroesToEnd(arr)));
+
+    }
+
 }
 // write a program that can move all the zeros to the end of an array
