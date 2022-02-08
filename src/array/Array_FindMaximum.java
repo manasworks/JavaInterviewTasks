ed8826490b14a2c0e40e43ee6464b0fd93d79ad3
Max Manas Kalenov <manasworks@gmai.com>
2022-02-07 21:31:46 -0600

    Question Find max from array

--- src/array/Array_FindMaximum.java
+++ src/array/Array_FindMaximum.java
@@ -4,6 +4,20 @@ public class Array_FindMaximum {
     public static void main(String[] args) {
 
         int[] arr = {0,5,6,9,7,2,-5,2,2-99,22,0};
+
+        // To find maximum from an array firstly we need one extra Variable to store the max number
+        // Than we will just run For Each Loop to check each element if it's bigger than our max value.
+        // And if it's Bigger than we will just reassign our Max with current each value.
+
+        int max = arr[0]; // our Variable that will hold max value. Assigning it with first number in Array. Anyway it will change will bigger number appears
+
+        for (int each : arr) { // Starting my for each loop
+            if (each>max){ // Condition if current number is bigger than Max
+                max = each; // If yes assigning new number as Max
+            }
+        }
+
+        System.out.println(max);
     }
 }
 //Write a function that can find the maximum number from an int Array
