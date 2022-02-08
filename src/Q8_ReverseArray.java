c62b90fda6f97d432492b7e00883c864dd6cecd2
Max Manas Kalenov <manasworks@gmai.com>
2022-02-07 19:51:49 -0600

    All questions

---
+++ src/Q8_ReverseArray.java
@@ -0,0 +1,9 @@
+public class Q8_ReverseArray {
+
+    public static void main(String[] args) {
+
+        char[] arr = {'A', 'B', 'C', 'D', 'E'};
+
+    }
+}
+// write a function that can reverse an array
