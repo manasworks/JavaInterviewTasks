316d4d6acd63682d62059b8e80ed8497264ef497
elvirka7 <elvirka7@gmail.com>
2022-02-07 21:22:23 -0500

    Array_FindMinimum task completed

--- src/array/Array_FindMinimum.java
+++ src/array/Array_FindMinimum.java
@@ -1,9 +1,23 @@
 package array;
 
+import java.util.Arrays;
+
 public class Array_FindMinimum {
     public static void main(String[] args) {
 
         int[] arr = {0,5,6,9,7,2,-5,2,2-99,22,0};
+        System.out.println("Minimum integer in array is: "+minArray(arr));
+    }
+
+    public static int minArray(int[] array) {
+        int min = Integer.MAX_VALUE;
+        for (int each : array) {
+            if (min > each) {
+                min = each;
+            }
+        }
+        return min;
     }
 }
+
 // Write a function that can find the minimum number from an int Array
