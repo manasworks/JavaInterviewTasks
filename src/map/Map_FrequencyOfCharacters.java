ae66ebf94dfcf0e7c90c58336a5c3c6313d1c202
Max Manas Kalenov <manasworks@gmai.com>
2022-02-08 09:15:41 -0600

    Questions update

--- src/map/Map_FrequencyOfCharacters.java
+++ src/map/Map_FrequencyOfCharacters.java
@@ -1,4 +1,14 @@
+/*
+Write a method that prints the frequency of each character from a String
+Input : str = "geeksforgeeks"
+Output : g2 e4 k2 s2 f1 o1 r1
+ */
+
 package map;
 
 public class Map_FrequencyOfCharacters {
+    public static void main(String[] args) {
+
+        String str = "";
+    }
 }
--- src/map/Map_SortByValues.java
+++ src/map/Map_SortByValues.java
@@ -1,4 +1,25 @@
+/*
+    Write a method that can sort the map by values
+    Input : Key = Math, Value = 98
+        Key = Data Structure, Value = 85
+        Key = Database, Value = 91
+        Key = Java, Value = 95
+        Key = Operating System, Value = 79
+        Key = Networking, Value = 80
+
+    Output : Key = Operating System, Value = 79
+         Key = Networking, Value = 80
+         Key = Data Structure, Value = 85
+         Key = Database, Value = 91
+         Key = Java, Value = 95
+         Key = Math, Value = 98
+ */
+
 package map;
 
 public class Map_SortByValues {
+    public static void main(String[] args) {
+
+
+    }
 }
