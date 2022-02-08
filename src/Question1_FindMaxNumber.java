afc8e960408ab3772eb89f23ef5dc4b027664b7d
Max Manas Kalenov <manasworks@gmai.com>
2022-02-07 19:35:53 -0600

    Initial Commit

--- src/Question1_FindMaxNumber.java
+++ src/Question1_FindMaxNumber.java
@@ -1,5 +1,7 @@
 public class Question1_FindMaxNumber {
     public static void main(String[] args) {
 
+        int[] arr = {0,5,6,9,7,2,-5,2,2-99,22,0};
     }
 }
+//Write a function that can find the maximum number from an int Array
