d1a3940a5a9f6843111070613e84e28c8aac2c6b
arstanbektemirov <abektemirov82@gmail.com>
2022-02-08 14:09:23 -0500

    Find_Max_inArray

--- src/array/Array_FindMaximum.java
+++ src/array/Array_FindMaximum.java
@@ -4,6 +4,13 @@ public class Array_FindMaximum {
     public static void main(String[] args) {
 
         int[] arr = {0,5,6,9,7,2,-5,2,2-99,22,0};
+        int max=arr[0];
+        for (int each : arr) {
+            if(each>max){
+                max=each;
+            }
+        }
+        System.out.println(max);
     }
 }
 //Write a function that can find the maximum number from an int Array
