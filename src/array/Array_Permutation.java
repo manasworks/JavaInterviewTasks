74cf778eae3613412f8915b226b44f0e345e0d43
Max Manas Kalenov <manasworks@gmai.com>
2022-02-08 22:07:47 -0600

    Permutation

--- src/array/Array_Permutation.java
+++ src/array/Array_Permutation.java
@@ -1,11 +1,36 @@
 package array;
 
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class Array_Permutation {
 
     public static void main(String[] args) {
 
-        char[] arr = {'B', 'D', 'A'};
+        char[] arr = {'A', 'B', 'C'};
+        giveAllCombinations(arr);
 
     }
+
+    public static void giveAllCombinations(char[] arr){
+        /*
+        To get all possible combination from array we need to create nested loops and how many nested loops we need is
+        depends on the number of values in array, or we can say the length of array. In our case we have 3 characters
+        that's why we will need 3 nested loops. And to avoid value duplicated and hold permutation we will check if in
+        sub nest value is not equal to upper
+         */
+
+        for (char each : arr) {
+            for (char sub : arr) {
+                if(each==sub) continue;
+                for (char last : arr) {
+                    if(sub==last || each==last) continue;
+                    System.out.println("["+each+", "+sub+", "+last+"]");
+                }
+            }
+        }
+    }
+
 }
 // Given an array of 3 characters print all permutation combinations from the given characters
