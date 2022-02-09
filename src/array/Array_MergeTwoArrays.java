87cd641e2493f141a6415cb0c090b6bb05170614
Max Manas Kalenov <manasworks@gmai.com>
2022-02-08 20:09:53 -0600

    Merge two Arrays

--- src/array/Array_MergeTwoArrays.java
+++ src/array/Array_MergeTwoArrays.java
@@ -7,9 +7,10 @@ public class Array_MergeTwoArrays {
     public static void main(String[] args) {
 
         int[] arr1 = {1,2,3,4,5};
-        int[] arr2 = {6,7,8,9,10};
+        int[] arr2 = {6,7,8,9,9,10};
 
         System.out.println(Arrays.toString(merge(arr1, arr2)));
+        System.out.println(Arrays.toString(merge2(arr1, arr2)));
 
     }
 
@@ -31,6 +32,26 @@ public class Array_MergeTwoArrays {
         }
         for (int each : arr2) { // Assigning second array to a new array. My index number becomes the length of first array
             result[index++]=each;
+        }
+        return result;
+    }
+
+    // Second approach with one loop
+    public static int[] merge2(int[] arr1, int[] arr2){
+
+        int ar1Len = arr1.length; // Getting length of 1 array
+        int ar2Len = arr2.length; // Getting length of 1 array
+        int loopMax = Math.max(ar1Len, ar2Len); // Getting length which is bigger
+
+        int[] result = new int[ar1Len+ar2Len]; // New array with length of two arrays
+
+        for (int i = 0; i < loopMax; i++) { // Running my loop according to the biggest array of two
+            if (i<ar1Len){ // Making sure that if the second array was longer we won't get out of bond exception
+                result[i] = arr1[i]; // Assigning new array
+            }
+            if (i<ar2Len){ // Making sure that if the first array was longer we won't get out of bond exception
+                result[ar1Len+i] = arr2[i]; // Assigning new array
+            }
         }
         return result;
     }
