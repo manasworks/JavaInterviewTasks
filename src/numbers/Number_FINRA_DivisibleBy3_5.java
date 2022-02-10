fcb511c49791834cf94b887c950bda23d643e304
elvirka7 <elvirka7@gmail.com>
2022-02-10 00:06:16 -0500

    Number_FINRA_DivisibleBy3_5 task completed

--- src/numbers/Number_FINRA_DivisibleBy3_5.java
+++ src/numbers/Number_FINRA_DivisibleBy3_5.java
@@ -1,4 +1,18 @@
 package numbers;
 
+import javax.management.StringValueExp;
+
 public class Number_FINRA_DivisibleBy3_5 {
+    public static void main(String[] args) {
+
+        finra1to30();
+    }
+
+    private static void finra1to30() {
+        for (int i = 1; i <= 30; i++) {
+            String result =(i%3==0 && i%5==0)? "FINRA" :(i%3==0)? "FIN" :(i%5==0)? "RA" : String.valueOf(i);
+            System.out.print(result+" ");
+
+        }
+    }
 }
