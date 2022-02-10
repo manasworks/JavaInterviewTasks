78a2301cca0700d6af788dd70339fcbc58800721
Max Manas Kalenov <manasworks@gmai.com>
2022-02-10 17:26:36 -0600

    Generate array sum up to zero

--- src/array/Array_SumUpTo0.java
+++ src/array/Array_SumUpTo0.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Array_SumUpTo0 {
     public static void main(String[] args) {
 
-        int num = 7;
+        int num = 33;
 
         System.out.println(Arrays.toString(generator(num)));
     }
