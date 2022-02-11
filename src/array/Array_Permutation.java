e8e685fd619498fd3f60f602ec111464861c9ebd
arstanbektemirov <abektemirov82@gmail.com>
2022-02-11 17:42:08 -0500

    ArraysSort

--- src/array/Array_Permutation.java
+++ src/array/Array_Permutation.java
@@ -1,10 +1,17 @@
 package array;
 
+
+import java.util.Arrays;
+
 public class Array_Permutation {
 
     public static void main(String[] args) {
 
         char[] arr = {'B', 'D', 'A'};
+
+        Arrays.sort(arr);//here we are calling from Java collections  Arrays sort method
+
+        System.out.println(Arrays.toString(arr));
 
     }
 }
