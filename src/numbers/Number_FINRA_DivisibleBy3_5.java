c87c9d582e36a9b8711a5f9315433d46bf0972e8
ZhyldyzAmanova <zhyldyzamanova05@gmail.com>
2022-02-12 01:33:59 -0500

    Finra divisible by 2 and 5 solved

--- src/numbers/Number_FINRA_DivisibleBy3_5.java
+++ src/numbers/Number_FINRA_DivisibleBy3_5.java
@@ -7,4 +7,26 @@ for numbers which are a multiple of both 3 and 5, print "FINRA" instead of the n
 package numbers;
 
 public class Number_FINRA_DivisibleBy3_5 {
+    public static void main(String[] args) {
+
+        System.out.println(Finra(30));
+
+    }
+
+    public static String Finra(int num){
+        String result = "";
+
+        for(int i = 1; i<=num; i++){
+            if(i%3==0 && i%5==0){
+                result+="FINRA"+" ";
+            } else if(i%3==0){
+                result+="FIN"+" ";
+            }else if(i%5==0){
+                result+="RA"+" ";
+            }else{
+                result+=i+" ";
+            }
+        }
+        return result;
+    }
 }
