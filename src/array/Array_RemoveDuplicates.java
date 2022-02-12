f126509112430cfb10911946b8ca01789805386a
arstanbektemirov <abektemirov82@gmail.com>
2022-02-11 20:43:24 -0500

    nothing change

--- src/array/Array_RemoveDuplicates.java
+++ src/array/Array_RemoveDuplicates.java
@@ -1,10 +1,29 @@
 package array;
 
+import java.util.Arrays;
+
 public class Array_RemoveDuplicates {
 
     public static void main(String[] args) {
 
         int[] arr = {1, 2, 2, 3, 4, 4, 4, 5, 5};
+
+        int newArr []= new int [arr.length];
+        int j=0;
+        int count=0;
+
+        for (int i = 0; i < arr.length-1; i++) {
+            if(arr[i]!=arr[i+1]){
+                count++;
+                newArr[j++]=arr[i];
+            }
+        }
+        newArr[j++]=arr[arr.length-1];
+
+
+        System.out.println(Arrays.toString(newArr));
+
+
 
     }
 }
