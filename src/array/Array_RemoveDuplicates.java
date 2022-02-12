3c0f96655bc279a7e10c745058c66cbb67e57d85
arstanbektemirov <abektemirov82@gmail.com>
2022-02-11 21:43:02 -0500

    nothing change

--- src/array/Array_RemoveDuplicates.java
+++ src/array/Array_RemoveDuplicates.java
@@ -6,6 +6,9 @@ public class Array_RemoveDuplicates {
 
     public static void main(String[] args) {
 
+    }
+
+    public int removeDublicate(int[] newArr){
         int[] arr = {1, 2, 2, 3, 4, 4, 4, 5, 5};
 
         int j=0;
@@ -18,19 +21,12 @@ public class Array_RemoveDuplicates {
         }
         int newArr []= new int [count];
         for (int i = 0; i < arr.length-1; i++) {
-           if(arr[i]!=arr[i+1])
-            newArr[j++]=arr[i];
+            if(arr[i]!=arr[i+1])
+                newArr[j++]=arr[i];
         }
-        newArr[j]= arr.length-1;
-        System.out.println(Arrays.toString(newArr));
-
-
-
-
-
-
-
-
+        newArr[j]= arr[arr.length-1];
+        return newArr;
     }
+
 }
 // Write a function that can remove the duplicates from an array of integers
