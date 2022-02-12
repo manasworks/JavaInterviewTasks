65c833c4be7c3899491c2d97e1e6405f84d7c242
Max Manas Kalenov <manasworks@gmai.com>
2022-02-11 21:00:34 -0600

    Question added

---
+++ src/string/String_SameLetters.java
@@ -0,0 +1,15 @@
+/*
+Write a return method that check if a string is build out of the same letters as another string.
+Ex:  same("abc",  "cab"); -> true
+same("abc",  "abb"); -> false:
+ */
+package string;
+
+public class String_SameLetters {
+    public static void main(String[] args) {
+
+        String str1="abc";
+        String str2="cab";
+
+    }
+}
