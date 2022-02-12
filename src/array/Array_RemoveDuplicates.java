500616919fc86fca891e99454e7cd0f6cb42fe9c
arstanbektemirov <abektemirov82@gmail.com>
2022-02-11 21:57:19 -0500

    nothing change

--- src/array/Array_RemoveDuplicates.java
+++ src/array/Array_RemoveDuplicates.java
@@ -5,28 +5,49 @@ import java.util.Arrays;
 public class Array_RemoveDuplicates {
 
     public static void main(String[] args) {
-
+        System.out.println("removeDublicate2() = " + removeDublicate2());
     }
 
-    public int removeDublicate(int[] newArr){
-        int[] arr = {1, 2, 2, 3, 4, 4, 4, 5, 5};
+    public static int[] removeDublicate(int[] arr) {
+        //int[] arr = {1, 2, 2, 3, 4, 4, 4, 5, 5};
 
-        int j=0;
-        int count=1;
+        int j = 0;
+        int count = 1;
 
-        for (int i = 0; i < arr.length-1; i++) {
-            if(arr[i]!=arr[i+1]){
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] != arr[i + 1]) {
                 count++;
             }
         }
-        int newArr []= new int [count];
-        for (int i = 0; i < arr.length-1; i++) {
-            if(arr[i]!=arr[i+1])
-                newArr[j++]=arr[i];
+        int newArr[] = new int[count];
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] != arr[i + 1])
+                newArr[j++] = arr[i];
         }
-        newArr[j]= arr[arr.length-1];
+        newArr[j] = arr[arr.length - 1];
         return newArr;
     }
 
+    public static int []removeDublicate2() {
+        int[] arr = {1, 2, 2, 3, 4, 4, 4, 5, 5};
+
+        int newArr[] = new int[arr.length];
+        int j = 0;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] != arr[i + 1]) {
+                newArr[j++] = arr[i];
+            }
+        }
+        newArr[j++] = arr[arr.length - 1];
+
+        for (int k = 0; k < j; k++) {
+             newArr[k]=newArr[j];
+        }
+       //return Arrays.toString(newArr);
+
+    }
+
 }
+
 // Write a function that can remove the duplicates from an array of integers
