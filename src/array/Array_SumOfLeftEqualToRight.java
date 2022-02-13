82016c71795b5aa8ccbf4d11332fde26d06c287a
arstanbektemirov <abektemirov82@gmail.com>
2022-02-13 16:15:25 -0500

    SumOfLeftEqualTiRight

--- src/array/Array_SumOfLeftEqualToRight.java
+++ src/array/Array_SumOfLeftEqualToRight.java
@@ -3,7 +3,7 @@ package array;
 public class Array_SumOfLeftEqualToRight {
     public static void main(String[] args) {
 
-        int[] arr = {7, 4, 4, 5, 6,7,2};
+        int[] arr = {6, 4, 4, 6,5, 6,7,2,3};
         sumOfLeftEqualToRight(arr);
     }
 
@@ -12,14 +12,14 @@ public class Array_SumOfLeftEqualToRight {
         int countRight = 0;
         int countLeft = 0;
 
-            for (int i = 0; i < arr.length / 2; i++) {
+            for (int i = 0; i < arr.length / 2; i++) {//creating first loop and counting up from right side to half of arr length
                 countLeft += arr[i];
             }
-            int j = arr.length / 2;
-            for (int k = arr.length - 1; k > j; k--) {
+        // int j = arr.length / 2;
+        for (int k = arr.length - 1; k >arr.length/2; k--) {//second loop, counting up from the end of length to half of arr
                 countRight += arr[k];
             }
-            if (countLeft == countRight) {
+            if (countLeft == countRight) {//if sum of right side equal to left side 
                 equalResult = true;
             } else {
                 equalResult = false;
