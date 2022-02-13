6737bf42c4beff3641bb815f95daab6029d84878
arstanbektemirov <abektemirov82@gmail.com>
2022-02-13 16:08:07 -0500

    SumOfLeftEqualTiRight

--- src/array/Array_SumOfLeftEqualToRight.java
+++ src/array/Array_SumOfLeftEqualToRight.java
@@ -3,32 +3,28 @@ package array;
 public class Array_SumOfLeftEqualToRight {
     public static void main(String[] args) {
 
-        int[] arr = {7, 4, 4, 5, 6,7};
+        int[] arr = {7, 4, 4, 5, 6,7,2};
         sumOfLeftEqualToRight(arr);
     }
 
     public static void sumOfLeftEqualToRight(int[] arr) {
+        boolean equalResult=false;
         int countRight = 0;
         int countLeft = 0;
-        for (int i = 0; i < arr.length / 2; i++) {
-            countLeft += arr[i];}
-            int j= arr.length/2;
-            for (int k = arr.length-1; k > j ; k--) {
+
+            for (int i = 0; i < arr.length / 2; i++) {
+                countLeft += arr[i];
+            }
+            int j = arr.length / 2;
+            for (int k = arr.length - 1; k > j; k--) {
                 countRight += arr[k];
             }
-            System.out.println(countLeft+" "+countRight);
-
-            /*for (int b = 0; b < arr.length; j++) {
-                if (countLeft == countRight) {
-                        break;
-                }
-                System.out.println(arr[b]);
-
-            }
-
-             */
-
-
+            if (countLeft == countRight) {
+                equalResult = true;
+            } else {
+                equalResult = false;
+        }
+        System.out.println(countLeft+" "+countRight+" \n "+equalResult);
 
         }
     }
