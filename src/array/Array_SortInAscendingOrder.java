238688e005bc893acd120a9ce30d380c1793c501
arstanbektemirov <abektemirov82@gmail.com>
2022-02-13 12:54:37 -0500

    Array Ascending

--- src/array/Array_SortInAscendingOrder.java
+++ src/array/Array_SortInAscendingOrder.java
@@ -8,20 +8,21 @@ public class Array_SortInAscendingOrder {
 
         int [] arr = {1,4,7,3,9,-4,-2,8,0};
         //Arrays.sort(arr);
-        System.out.println(Arrays.toString(sortAscending(arr)));
+        System.out.println(Arrays.toString(AscendingSort(arr)));
 
     }
 
-    public static int[] sortAscending(int[] arr){
+    public static int[] AscendingSort(int[] arr){
 
+// for ascending sort we need to creat to loops, one main loop and second one nested loop.
 
-        for (int i = 0; i < arr.length-1; i++) {
-            int temp = 0;
-            for (int k = i + 1; k < arr.length - 1; k++) {
-                if (arr[i] > arr[k]) {
-                    temp = arr[i];
-                    arr[i] = arr[k];
-                    arr[k] = temp;
+        int temp = 0;// also, we need create a temporary storage for swapping numbers
+        for (int i = 0; i < arr.length; i++) { //first main loop give us first element from arr
+            for (int k = i + 1; k < arr.length; k++) {//nested loop, compare each element from main loop with every next element from arr
+                if (arr[i] > arr[k]) {//if element bigger then next element
+                    temp = arr[i];//we need to put bigger element in our temp container
+                    arr[i] = arr[k];//replace smaller element with bigger element
+                    arr[k] = temp;//take from temp container bigger and put him next to smaller element
                 }
             }
         }
