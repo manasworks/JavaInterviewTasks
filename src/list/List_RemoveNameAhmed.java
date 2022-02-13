e1f7743b71e710a782c631b328318b81e82f7151
Max Manas Kalenov <manasworks@gmai.com>
2022-02-13 15:42:10 -0600

    List remove Ahmet

--- src/list/List_RemoveNameAhmed.java
+++ src/list/List_RemoveNameAhmed.java
@@ -1,4 +1,22 @@
 package list;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class List_RemoveNameAhmed {
+    public static void main(String[] args) {
+        ArrayList <String> list = new ArrayList<>(Arrays.asList("Ahmet", "Mehmet", "Ahmet", "John", "Bob", "Ahmet"));
+
+        System.out.println(removeString(list, "Ahmet"));
+    }
+
+    public static ArrayList <String> removeString(ArrayList<String> list, String name){
+        // Simple we're just using removeIf method
+        list.removeIf(p-> p.equals(name));
+        return list;
+    }
 }
+/*
+    Given a list of people' names: "Ahmed", "John", Eric", "Ahmed".....
+    Write a java operation to remove all the names named Ahmed
+ */
