b30320836e17c16de17a85d397a26ad0a8117392
Max Manas Kalenov <manasworks@gmai.com>
2022-02-13 19:18:57 -0600

    Numbers decimal to binary

--- src/numbers/Number_BinaryRepresentationOfN.java
+++ src/numbers/Number_BinaryRepresentationOfN.java
@@ -1,4 +1,32 @@
+/*
+return the number of '1's in the binary representation of n for any integer n, where n > 0
+Example:
+    for n=6 the binary representation is '110' and the number of '1's in that representation is 2
+ */
+
 package numbers;
 
 public class Number_BinaryRepresentationOfN {
+    public static void main(String[] args) {
+        int num=6;
+        System.out.println( convertToBinary(num));
+    }
+    /*
+    Decimal (10) > Binary (2)
+                0=0
+                1=1
+                2=10
+                3=11
+                ...
+                Basically we have our decimal divided by 2 and remainder 1 or 0 is or digit
+     */
+
+    public static String convertToBinary(int num){
+        String binary=""; // To hold binary value
+        while (num>0){ // Do the code while my num still bigger than 0
+            binary=num%2+binary; // Getting remainder of num and adding it to left side of value.
+            num= num/2; // Dividing my num by 2
+        }
+        return binary;
+    }
 }
