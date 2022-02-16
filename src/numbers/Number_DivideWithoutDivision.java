30973ccdae6a2d149adc7bbb27a51cc0ff52d007
Max Manas Kalenov <manasworks@gmai.com>
2022-02-16 09:01:05 -0600

    Numbers divide without division

--- src/numbers/Number_DivideWithoutDivision.java
+++ src/numbers/Number_DivideWithoutDivision.java
@@ -1,4 +1,29 @@
+/*
+Write a method that can divide two numbers without using division operator
+ */
+
 package numbers;
 
 public class Number_DivideWithoutDivision {
+    public static void main(String[] args) {
+        int a=15;
+        int b=2;
+
+        System.out.println("divide("+a+"/"+b+") = " + divide(a, b));
+
+    }
+
+    public static double divide(int a, int b){
+        double num1=0, num2=0.0;
+        double remainder=a%b*10;
+        while (a>=b){
+            a-=b;
+            num1++;
+        }
+        while (remainder>=b){
+            remainder-=b;
+            num2+=0.1;
+        }
+        return num1+num2;
+    }
 }
