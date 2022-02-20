42497a5be7230b57195f60fe71a50ec39c950da1
Asyl Orozmatova <asylumaraly@gmail.com>
2022-02-20 16:02:13 -0500

    Array min number function

--- src/array/Array_FindMinimum.java
+++ src/array/Array_FindMinimum.java
@@ -4,6 +4,14 @@ public class Array_FindMinimum {
     public static void main(String[] args) {
 
         int[] arr = {0,5,6,9,7,2,-5,2,2-99,22,0};
+        int min=arr[0];
+
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]<min){
+                min=arr[i];
+            }
+        }
+        System.out.println(min);
     }
 }
 // Write a function that can find the minimum number from an int Array
