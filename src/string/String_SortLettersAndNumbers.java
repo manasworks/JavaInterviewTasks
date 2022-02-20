1963536fb97e4f3c8ba942c16eb256ffaa2bd842
Max Manas Kalenov <manasworks@gmai.com>
2022-02-20 16:19:51 -0600

    Question sort letters and numbers

--- src/string/String_SortLettersAndNumbers.java
+++ src/string/String_SortLettersAndNumbers.java
@@ -16,22 +16,29 @@ public class String_SortLettersAndNumbers {
         System.out.println( sort(str) );
     }
 
+    /*
+    First we need to split the string by part of letters and numbers. Then we will take each part and sort it.
+    To sort I will use arrays sort method. To do so will convert my part to array sort it and then concate it back
+    to my result string
+     */
+
     public static String sort(String str){
 
-        String result="";
+        String result=""; // To save my result
 
-        for (int i = 0; i < str.length(); i++) {
+        for (int i = 0; i < str.length(); i++) { // First main loop to iterate each char
 
-            String temp="";
-            if (Character.isLetter(str.charAt(i))){
-                while (i<str.length() && Character.isLetter(str.charAt(i))){
-                    temp+=str.charAt(i++);
+            String temp=""; // Temp String to save my part of a String
+
+            if (Character.isLetter(str.charAt(i))){ // Will check if current char is Letter.
+                while (i<str.length() && Character.isLetter(str.charAt(i))){ // Will start my while loop till my char is still a letter
+                    temp+=str.charAt(i++); // Adding my letter to temp
                 }
-                char[] chars = temp.toCharArray();
-                Arrays.sort(chars);
-                result += new String(chars);
-                i--;
-            } else {
+                char[] chars = temp.toCharArray(); // Converting my temp result to a char array
+                Arrays.sort(chars); // Sorting my char array
+                result += new String(chars); // Putting sorted temp to my result
+                i--; // Making my i minus one to prevent jumping over.
+            } else { // If current char not a letter do same
                 while (i<str.length() && Character.isDigit(str.charAt(i))){
                     temp+=str.charAt(i++);
                 }
@@ -41,6 +48,6 @@ public class String_SortLettersAndNumbers {
                 i--;
             }
         }
-        return result;
+        return result; // return my result
     }
 }
