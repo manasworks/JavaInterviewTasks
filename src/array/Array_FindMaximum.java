e3c6da741940358bb7b9ae449f76c4ab4bdb8a94
Asyl Orozmatova <asylumaraly@gmail.com>
2022-02-20 13:49:18 -0500

    Array max number function

--- src/array/Array_FindMaximum.java
+++ src/array/Array_FindMaximum.java
@@ -4,6 +4,14 @@ public class Array_FindMaximum {
     public static void main(String[] args) {
 
         int[] arr = {0,5,6,9,7,2,-5,2,2-99,22,0};
+        int max=arr[0];
+
+        for (int i=0; i<arr.length; i++){
+            if(arr[i]>max){
+                max=arr[i];
+            }
+        }
+        System.out.println(max);
     }
 }
 //Write a function that can find the maximum number from an int Array
