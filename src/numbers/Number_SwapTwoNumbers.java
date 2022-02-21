49046afc12aab3f9a4c46cc4aa95015805c0f03b
Max Manas Kalenov <manasworks@gmai.com>
2022-02-21 10:30:53 -0600

    Number sum of the all digits

--- src/numbers/Number_SwapTwoNumbers.java
+++ src/numbers/Number_SwapTwoNumbers.java
@@ -1,4 +1,18 @@
+/*
+Swap two variable' values without using a third variable
+ */
 package numbers;
 
 public class Number_SwapTwoNumbers {
+    public static void main(String[] args) {
+        int a=15;
+        int b=10;
+
+        a=a+b;
+        b=a-b;
+        a=a-b;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+    }
 }
