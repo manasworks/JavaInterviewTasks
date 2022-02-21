cc4861c05edcc7ce2047ace17e778cde9de5f4a6
Max Manas Kalenov <manasworks@gmai.com>
2022-02-20 22:20:36 -0600

    Question Reverse the number

--- src/numbers/Number_ReverseInteger.java
+++ src/numbers/Number_ReverseInteger.java
@@ -1,4 +1,20 @@
 package numbers;
 
 public class Number_ReverseInteger {
+    public static void main(String[] args) {
+
+        int num=1982;
+        System.out.println( reverNum(num) );
+    }
+
+    public static int reverNum(int num){
+        int reversed=0;
+
+        while (num>0){
+            reversed=reversed*10+num%10;
+            num= num/10;
+        }
+
+        return reversed;
+    }
 }
