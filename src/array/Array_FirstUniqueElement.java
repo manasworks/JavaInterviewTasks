5c07d943ee2c015c2ecdde28e0b99a68b5112622
Asyl Orozmatova <asylumaraly@gmail.com>
2022-02-22 14:58:19 -0500

    Array first

--- src/array/Array_FirstUniqueElement.java
+++ src/array/Array_FirstUniqueElement.java
@@ -4,23 +4,11 @@ public class Array_FirstUniqueElement {
     public static void main(String[] args) {
 
         int[] arr = {0, 5, 6, 9, 7, 2, -5, 2, 2 - 99, 22, 0};
-        int frequency = 0;
-        for (int i = 0; i < arr.length; i++) {
-
-            int element = arr[i];
-
-            for (int j = 0; j < arr.length; j++) {
-                if (arr[i] == element) {
-                    frequency++;
-                } if (frequency >1) {
-                    System.out.println(element);
-            }
 
 
-            }
-        }
     }
 }
+
 
 
 
