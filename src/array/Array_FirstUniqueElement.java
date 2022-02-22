448a75223d5d067e9d607542c9ab1b9847f5f691
Asyl Orozmatova <asylumaraly@gmail.com>
2022-02-22 16:00:19 -0500

    Array_FirstUniqueElement and Array_MergeTwoArrays

--- src/array/Array_FirstUniqueElement.java
+++ src/array/Array_FirstUniqueElement.java
@@ -6,6 +6,19 @@ public class Array_FirstUniqueElement {
         int[] arr = {0, 5, 6, 9, 7, 2, -5, 2, 2 - 99, 22, 0};
 
 
+        for (int each : arr) {
+            int frequency = 0;
+            for (int element : arr) {
+                if (element == each) {
+                    frequency++;
+                }
+
+            }
+            if (frequency == 2) {
+                System.out.println(each);
+                break;
+            }
+        }
     }
 }
 
--- src/array/Array_MergeTwoArrays.java
+++ src/array/Array_MergeTwoArrays.java
@@ -1,12 +1,28 @@
 package array;
 
+import java.util.Arrays;
+
 public class Array_MergeTwoArrays {
 
     public static void main(String[] args) {
 
-        int[] arr1 = {1,2,3,4,5};
-        int[] arr2 = {6,7,8,9,10};
+        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] arr2 = {6, 7, 8, 9, 10};
+        int[] newArray = new int[arr1.length + arr2.length];
 
+
+        int i = 0;
+        for (int each : arr1) { //1,2,3,4,5
+            newArray[i++] = each;
+        }
+
+
+        for (int each : arr2) {
+            newArray[i++] = each;
+        }
+
+        System.out.println(Arrays.toString(newArray));
     }
 }
+
 // Write a return method that can concat two arrays
