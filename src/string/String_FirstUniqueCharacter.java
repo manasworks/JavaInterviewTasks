30319ef28293e8d72f3e4a3713c4ab75b93ba4c1
Max Manas Kalenov <manasworks@gmai.com>
2022-02-22 20:08:14 -0600

    Strings first unique char

--- src/string/String_FirstUniqueCharacter.java
+++ src/string/String_FirstUniqueCharacter.java
@@ -1,4 +1,23 @@
+/*
+write a program that can return the first non-repeated character from a string
+ */
 package string;
 
 public class String_FirstUniqueCharacter {
+    public static void main(String[] args) {
+        String str="aasdsdmklrktlrA";
+        System.out.println("First unique char is: " + firstUnique(str));
+    }
+
+    public static char firstUnique(String str){
+
+        for (int i = 0; i < str.length(); i++) {
+            int count=0;
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(i)==str.charAt(j)) count++;
+            }
+            if (count==1) return str.charAt(i);
+        }
+        return 'n';
+    }
 }
