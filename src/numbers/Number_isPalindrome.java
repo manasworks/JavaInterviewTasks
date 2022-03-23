01ed667e1dab83aed4161d24cedc4fd6362bc6c5
Manas Kalenov <manasworks@gmail.com>
2022-03-23 11:54:02 -0500

    Number is Palindrome

---
+++ src/numbers/Number_isPalindrome.java
@@ -0,0 +1,12 @@
+/*
+Write a method that will check if a number is palindrome
+               Ex: isPalindorme(1221)-> True
+                   isPalindrome(2132)-> False
+ */
+package numbers;
+
+public class Number_isPalindrome {
+    public static void main(String[] args) {
+
+    }
+}
