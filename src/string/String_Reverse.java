c6ec8c4bbb6dac95db9a0da6d534d69382a60233
Manas Kalenov <manasworks@gmail.com>
2022-04-14 17:22:36 -0500

    String reverse

--- src/string/String_Reverse.java
+++ src/string/String_Reverse.java
@@ -1,16 +1,28 @@
 package string;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class String_Reverse {
     public static void main(String[] args) {
 
-        Scanner sc=new Scanner(System.in);
-        String A=sc.next();
-        String B=new StringBuilder(A).reverse().toString();
+        String[] array = {"Ram", "Sam", "Ram", "Sam", "Cam", "Tam"};
+        Map<String, Integer> map = new HashMap<>();
+        for (String each : array) {
+            int freq = Collections.frequency(Arrays.asList(array), each);
+            map.put(each, freq);
+        }
+        System.out.println("Map = "+map);
 
+        String result="";
+        for (String s : array) {
+            int count=0;
+            for (String e : array) {
+                if (s.equals(e)) count++;
+            }
+            if(!result.contains(s)) result+=s+"="+count+" ";
+        }
 
-        System.out.println("B = " + B);
+        System.out.println("result = " + result);
 
     }
 }
